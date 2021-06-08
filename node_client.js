0547c7f613c39f1cdb015e1ead16ce1f34e7935f
MariaSereti <mariasereti@csd.auth.gr>
2021-06-08 20:39:01 +0300

    Test CORS functionality

--- node_client.js
+++ node_client.js
@@ -30,6 +30,12 @@ var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 var PORT = process.env.PORT || 3000;
 
 app.post('/signup', function (req, res) {
